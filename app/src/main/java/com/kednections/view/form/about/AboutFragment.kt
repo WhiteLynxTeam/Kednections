62817e0e5e5f71025bce322772378bf7c8fbfbdb
Yuris <yuris.musin@mail.ru>
2025-07-28 18:34:16 +0500

    добавил фрагменты выбора общения и о себе

--- app/src/main/java/com/kednections/view/form/about/AboutFragment.kt
+++ app/src/main/java/com/kednections/view/form/about/AboutFragment.kt
@@ -1,22 +1,22 @@
-package com.kednections.view.form.geolocation
+package com.kednections.view.form.about
 
 import android.content.Context
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
-import androidx.navigation.fragment.findNavController
-import com.kednections.R
-import com.kednections.databinding.FragmentGeolocationBinding
+import com.kednections.databinding.FragmentAboutBinding
 import com.kednections.utils.startMarquee
 import com.kednections.view.activity.FormActivityViewModel
 import dagger.android.support.AndroidSupportInjection
 
-class GeolocationFragment : Fragment() {
+class AboutFragment : Fragment() {
 
-    private var _binding: FragmentGeolocationBinding? = null
+    private var _binding: FragmentAboutBinding? = null
     private val binding get() = _binding!!
     private val viewModel: FormActivityViewModel by activityViewModels()
 
@@ -30,7 +30,7 @@ class GeolocationFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
 
-        _binding = FragmentGeolocationBinding.inflate(inflater, container, false)
+        _binding = FragmentAboutBinding.inflate(inflater, container, false)
         return binding.root
     }
 
@@ -40,10 +40,18 @@ class GeolocationFragment : Fragment() {
         //бегущая строка (Анимация)
         startMarquee(binding.textDescription, binding.textHorizontalScroll, speed = 5000L)
 
-        binding.btnResume.setOnClickListener {
-            findNavController().navigate(R.id.action_geolocationFragment_to_purposesFragment)
-            viewModel.increaseProgress()
-        }
+        binding.etAbout.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                val currentLength = s?.length ?: 0
+                binding.tvCharCount.text = "$currentLength/200"
+
+                binding.btnResume.isEnabled = currentLength >= 1
+            }
+
+            override fun afterTextChanged(s: Editable?) {}
+        })
     }
 
 }
