2ec46eafe173ba093c374354597e95da05dc618c
Mea <maevski.ed@gmail.com>
2025-07-28 15:02:23 +0300

    add di SharedPreferences, usecase, repository, storage.

--- app/src/main/java/com/kednections/di/modules/MainModule.kt
+++ app/src/main/java/com/kednections/di/modules/MainModule.kt
@@ -6,6 +6,7 @@ import com.kednections.view.form.avatar.AvatarFragment
 import com.kednections.view.form.geolocation.GeolocationFragment
 import com.kednections.view.form.nickname.NickNameFragment
 import com.kednections.view.form.specialization.SpecializationFragment
+import com.kednections.view.welcome.WelcomeFragment
 import dagger.Module
 import dagger.android.ContributesAndroidInjector
 
@@ -28,5 +29,8 @@ interface MainModule {
 
     @ContributesAndroidInjector
     fun bindGeolocationFragment(): GeolocationFragment
+
+    @ContributesAndroidInjector
+    fun bindWelcomeFragment(): WelcomeFragment
 
 }
