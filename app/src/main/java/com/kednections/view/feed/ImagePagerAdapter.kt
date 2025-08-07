ff7c99492090c74cc875f51b7b991e6d33dc9116
Yuris <yuris.musin@mail.ru>
2025-08-07 18:00:26 +0500

    доработал функционал на экране с витринами, подправил UI

--- app/src/main/java/com/kednections/view/feed/ImagePagerAdapter.kt
+++ app/src/main/java/com/kednections/view/feed/ImagePagerAdapter.kt
@@ -6,6 +6,7 @@ import android.view.ViewGroup
 import android.widget.ImageView
 import androidx.recyclerview.widget.RecyclerView
 import com.kednections.R
+import com.kednections.domain.models.feed.ImageDetail
 
 class ImagePagerAdapter(
     private val images: List<ImageDetail>,
